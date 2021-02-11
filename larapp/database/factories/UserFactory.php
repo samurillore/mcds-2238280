0a95b8eed3f8a9673f719848275517f3bd280ea6
samurillore <s51@misena.edu.co>
2021-02-10 21:08:13 -0500

    Reto Factories 1

--- larapp/database/factories/UserFactory.php
+++ larapp/database/factories/UserFactory.php
@@ -5,9 +5,6 @@ namespace Database\Factories;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
-use Faker\Generator as Faker;
-use App\Post;
-use Illuminate\Http\File;
 
 class UserFactory extends Factory
 {
@@ -25,30 +22,28 @@ class UserFactory extends Factory
      */
     public function definition()
     {
-        $gender="Female";
-        //$image = $faker->image();
-        //$imageFile = new File($image);
-        if ($gender=="Male") {
-            $name="firstNameMale";
-            # code...
-        }
-        else
-        {
-            $name="firstNameFemale";
-        }
-
+        $gen= $this->faker->randomElement($array = array('female', 'male'));
+        $fn=$this->faker->firstname($gen);
+        $ln=$this->faker->lastname();
+        $wn=$fn." ".$ln;
+        $email=$fn.".".$ln."@".$this->faker->randomElement($array = array('gmail.com', 'misena.edu.co'));
         return [
-            'fullname'          => $this->faker->$name,
-            'email'             => $this->faker->unique()->safeEmail,
+            'gender'            =>$gen,     
+           //'fullname'          => $this->faker->firstname($gen).faker->lastname($gen),
+            'fullname'          =>$wn,
+            //'email'             => $this->faker->unique()->safeEmail,
+            'email'             =>$email,
             'phone'             => $this->faker->numberBetween(3101000000, 3202000000) ,
             'birthdate'         => $this->faker->dateTimeBetween('1960-01-01', '1999-12-31'),
-            'gender'            => $gender,
+
             'address'           => $this->faker->streetAddress,
-            'photo'             => $this->faker->imageUrl(storage_path('app/public/imgs')),
+            //'photo'             => $this->faker->imageUrl(storage_path('app/public/imgs'),$width = 640, $height = 480,'people'),
+            //'photo'             => $this->faker->imageUrl(storage_path('app/public/imgs')),
+            'photo'             =>'http://lorempixel.com/800/600/people/',
             'role'              => 'Editor',      
             'email_verified_at' => now(),
             'password'          => bcrypt('editor'), 
-            'remember_token'    => Str::random(10),            
+            'remember_token'    => Str::random(10),
         ];
     }
 }
