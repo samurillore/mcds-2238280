4432a0f67fa50160078e854bfb63236e92f0f270
samurillore <s51@misena.edu.co>
2021-02-16 22:36:18 -0500

    Reto2

---
+++ larapp/resources/views/showuser.blade.php
@@ -0,0 +1,8 @@
+<p>{{ $user->name}}</p>
+
+<hr>
+
+<p>{{ $user->phone }}</p>
+<p>{{ $user->address }}</p>
+<p>{{ $user->gender }}</p>
+<p>{{ $user->role }}</p>
--- larapp/routes/web.php
+++ larapp/routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Http\Request;
+use Carbon\Carbon;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +16,37 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('hello', function () {
+    return "<h1> Welcome to the Himalaya</h1>";
+});
+
+Route::get('allusers', function () {
+    $users = App\Models\User::take(5)->get();
+    //var_dump($users);
+    dd($users);
+});
+
+Route::get('showuser/{id}', function (Request $request) {
+    $id=$request->id;
+    $user = App\Models\User::find($id);
+    return view('showuser')->with('user',$user);
+});
+
+Route::get('reto/{id}', function (Request $request) {
+    $id=$request->id;
+    $today = Carbon::now();
+    $info =array();
+    for ($i=$id; $i < $id+10; $i++) { 
+    	$user = App\Models\User::find($i);
+    	$date  = Carbon::createFromTimestamp(strtotime($user->created_at));
+    	$diff_in_weeks=$date->diffInWeeks($today);
+    	$edad = Carbon::createFromDate($user->birthdate)->age;
+    	$info[]=$user->fullname.'-'.$edad.'years old - '.'created '.$diff_in_weeks.' weeks ago';
+    }
+    dd($info);
+    //$date = Carbon::now()->subYears(2);
+	//$creado = Carbon::createFromDate($acreado)->age;
+	//Carbon::createFromTimestamp($user->created_at)->week
 });
